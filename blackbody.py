c1b85d3038a81644e4d183aa5d866b3593e7dd39
azzorini <43655964+azzorini@users.noreply.github.com>
2022-09-27 12:13:02 +0200

    Actualización color
    Actualización del método por el cual se determina el color de una estrella

--- blackbody.py
+++ blackbody.py
@@ -7,7 +7,11 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
 from scipy.constants import h, c, k, Wien
+from scipy.integrate import simpson
 import numpy as np
+
+def piecewise_gaussian(x, mu, sigma1, sigma2):
+    return np.piecewise(x, [x < mu, x >= mu], [lambda x: np.exp(-0.5*(x-mu)**2/sigma1**2), lambda x: np.exp(-0.5*(x-mu)**2/sigma2**2)])
 
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
@@ -72,18 +76,30 @@ class Ui(QtWidgets.QMainWindow):
 
         x = msg.exec_()
 
-    def blackbody_I(self, landa_nm, T):
+    @staticmethod
+    def blackbody_I(landa_nm, T):
         landa = landa_nm*1e-9
         return 2*h*c**2/landa**5/(np.exp(h*c/landa/k/T) - 1)
 
-    # Aproximación muy cutre que se podría mejorar bastante
-    def blackbody_rgb_string(self, T):
-        colors = np.array([self.blackbody_I(660, T), self.blackbody_I(560, T), self.blackbody_I(500, T)])
-        colors *= 255/max(colors)
+    @staticmethod
+    def blackbody_rgb_string(T):
+        landas = np.linspace(380, 780, 100)
+        # Aproximaciones
+        x_bar = 1.056*piecewise_gaussian(landas, 599.8, 37.9, 31.0) + 0.362*piecewise_gaussian(landas, 442., 16, 26.7) - 0.065*piecewise_gaussian(landas, 501.1, 20.4, 26.2)
+        y_bar = 0.821*piecewise_gaussian(landas, 568.8, 46.9, 40.5) + 0.286*piecewise_gaussian(landas, 530.9, 16.3, 31.1)
+        z_bar = 1.217*piecewise_gaussian(landas, 437., 11.8, 36.) + 0.681*piecewise_gaussian(landas, 459., 26., 13.8)
+
+        L = Ui.blackbody_I(landas, T)
+        XYZ = np.array([simpson(L*x_bar, landas), simpson(L*y_bar, landas), simpson(L*z_bar, landas)])
+
+        matrix_to_rgb = np.array([[8041697, -3049000, -1591847], [-1752003, 4851000, 301853], [17697, -49000, 3432153]])/3400850.
+
+        RGB = matrix_to_rgb.dot(XYZ)
+        RGB *= 255/max(RGB)
 
         # Convertimos el resultado a string
         s = ""
-        for v in colors:
+        for v in RGB:
             s += hex(int(v))[2:]
         return s
 
